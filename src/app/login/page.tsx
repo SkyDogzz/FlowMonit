c5638858b9cd49b50b523ba363dde677fe04d072
SkyDogzz <thomas.stephan@live.fr>
2024-05-07 19:50:23 +0200

    Init project

---
+++ src/app/login/page.tsx
@@ -0,0 +1,74 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+import { signIn, useSession } from 'next-auth/react';
+
+export default function LoginPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const { data: session, status } = useSession();
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const result = await signIn('credentials', {
+            redirect: false,
+            email,
+            password
+        });
+
+        if (result && result.error) {
+            setError(result.error);
+            const interval = setInterval(() => {
+                setError('');
+                clearInterval(interval);
+            }, 20000);
+            console.error("Failed to sign in!", result.error);
+        } else {
+            console.log("Logged in successfully!");
+            window.location.href = "/";
+        }
+    };
+
+    useEffect(() => {
+        if (status === "authenticated") {
+            window.location.href = "/";
+        }
+    }, [status, session]);
+
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <form onSubmit={handleLogin} className='flex flex-col gap-4 bg-gray-200 dark:bg-gray-800 p-4 rounded-md dark:text-white w-3/4 sm:w-2/3 md:w-1/2'>
+                {error &&
+                    <div role="alert" className="alert alert-error">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                        <span>Failed to sign in: email or password incorrect</span>
+                    </div>
+                }
+                <div className='flex flex-col gap-2'>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        className='p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:border-gray-700'
+                    />
+                </div>
+                <div className='flex flex-col gap-2'>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        className='p-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:border-gray-700'
+                    />
+                </div>
+                <button type="submit" className='p-2 bg-blue-500 text-white rounded-md dark:bg-blue-900'>Register</button>
+            </form>
+        </div>
+    )
+}
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,61 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import type { JWT } from "next-auth/jwt";
+import type { Session } from "next-auth";
+import bcrypt from "bcrypt";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+    
+interface User {
+    id: string;
+    email: string;
+}
+
+export default NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials: Record<"email" | "password", string> | undefined, req: any): Promise<User | null> {
+                if (credentials === undefined) {
+                    return null;
+                }
+
+                const user = await prisma.user.findFirst({
+                    where: {
+                        email: credentials.email
+                    }
+                });
+                if (user && await bcrypt.compare(credentials.password, user.password)) {
+                    return { id: user.id.toString(), email: user.email } as User;
+                }
+                return null;
+            }
+        })
+    ],
+    session: {
+        strategy: "jwt",
+        maxAge: 60 * 60
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        },
+        async session({ session, token }: { session: Session; token: JWT }) {
+            if (session.user && token.id) {
+                session.user.id = token.id as string;
+            }
+            return session;
+        }
+    },
+    pages: {
+        signIn: "/login",
+    }
+});
